ed89cc1007d7f2f11f36eb88295610fd9404019b
joe palfrey <joepalfrey@gmail.com>
2020-03-14 16:07:18 +0100

    added positionable behavior

---
+++ src/Model/Behavior/PositionableBehavior.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Model\Behavior;
+
+use Cake\ORM\Behavior;
+use ArrayObject;
+use Cake\Datasource\EntityInterface;
+use Cake\Event\Event;
+use Cake\ORM\Query;
+
+class PositionableBehavior extends Behavior
+{
+
+	protected $_defaultConfig = [
+		'field' => 'position',
+	];
+
+	public function findPosition(Query $query, array $options)
+	{
+		$config = $this->config();
+		return $query->order([$config['field'] => 'ASC']);
+	}
+
+	public function beforeSave(Event $event, EntityInterface $entity, ArrayObject $options)
+	{
+
+		$config = $this->config();
+
+		if ($entity->isNew()) {
+			$max_position = $this->_table->find()
+				->select([
+					$config['field'] => 'MAX(' . $config['field'] . ')'
+				])
+				->first();
+
+			$entity->{$config['field']} = $max_position[$config['field']] + 1;
+		}
+	}
+}
