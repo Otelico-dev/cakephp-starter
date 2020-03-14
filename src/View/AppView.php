f28775fc5c74ed05e016fc1f7d1a5522c452c536
joe palfrey <joepalfrey@gmail.com>
2020-03-14 16:08:33 +0100

    added datatables reorder helper

--- src/View/AppView.php
+++ src/View/AppView.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
@@ -11,6 +12,7 @@
  * @since     3.0.0
  * @license   https://opensource.org/licenses/mit-license.php MIT License
  */
+
 namespace App\View;
 
 use Cake\View\View;
@@ -25,16 +27,17 @@ use Cake\View\View;
 class AppView extends View
 {
 
-    /**
-     * Initialization hook method.
-     *
-     * Use this method to add common initialization code like loading helpers.
-     *
-     * e.g. `$this->loadHelper('Html');`
-     *
-     * @return void
-     */
-    public function initialize()
-    {
-    }
+	/**
+	 * Initialization hook method.
+	 *
+	 * Use this method to add common initialization code like loading helpers.
+	 *
+	 * e.g. `$this->loadHelper('Html');`
+	 *
+	 * @return void
+	 */
+	public function initialize()
+	{
+		$this->loadHelper('DtReorder');
+	}
 }
---
+++ src/View/Helper/DtReorderHelper.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\View\Helper;
+
+use Cake\View\Helper;
+
+use Cake\Routing\Router;
+
+class DtReorderHelper extends Helper
+{
+	public function getScript($table_id, $reorder_url)
+	{
+
+		$reorder_url = Router::url($reorder_url);
+
+		return <<<EOT
+		$('#$table_id').on('row-reorder.dt', function(dragEvent, data, nodes) {
+
+		var updated_rows = [];
+
+		for (i = 0; i < data.length; i++) {
+			updated_rows.push({
+				id: $(data[i].node.cells[0]).text(),
+				new_position: data[i].newPosition
+			});
+		}
+		if (updated_rows.length) {
+			$.post('$reorder_url', {
+				data: updated_rows
+			}, function(data) {
+				$('#$table_id').dataTable().api().draw(false);
+			});
+		}
+	});
+EOT;
+	}
+}
